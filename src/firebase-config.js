932e92f57c7dabb3250fcbf8cdd7f95102473be4
zachvlat <zatsando@protonmail.com>
2022-06-10 14:58:39 +0300

    lol

---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh-7TSUhYkV64GqwPxP7oDyqOHy4s7DSs",
+  authDomain: "zatsblog-490c4.firebaseapp.com",
+  projectId: "zatsblog-490c4",
+  storageBucket: "zatsblog-490c4.appspot.com",
+  messagingSenderId: "660442845810",
+  appId: "1:660442845810:web:0981a5669664ce3b17a8eb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
