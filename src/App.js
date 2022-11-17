98eb6708d52a3db62264a4f2e8bf2e37bf89695c
zachvlat <zachariasvlatakis@gmail.com>
2022-11-17 16:06:05 +0200

    socials and other changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import React, { useState } from "react";
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to='/'> ZATS BLOG (guides and stuff) </Link>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <div className='mainDiv'>
+      <Router>
+        <nav>
+          <Link to='/'> ZATS BLOG (guides and stuff)</Link>
+        </nav>
+        <div className='socials'>
+          <a href='https://github.com/zachvlat'>
+            <img
+              src='https://img.icons8.com/nolan/40/github.png'
+              alt='github'
+            />
+          </a>
+          <a href='https://www.linkedin.com/in/zvlatakis/'>
+            <img
+              src='https://img.icons8.com/nolan/40/1A6DFF/C822FF/linkedin-circled.png'
+              alt='linkedin'
+            />
+          </a>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
