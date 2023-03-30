b0e9f6e99559b80d8b1436e64558114b14a6e3b3
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-03-30 08:08:42 +0000

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCh-7TSUhYkV64GqwPxP7oDyqOHy4s7DSs",
-  authDomain: "zatsblog-490c4.firebaseapp.com",
-  projectId: "zatsblog-490c4",
-  storageBucket: "zatsblog-490c4.appspot.com",
-  messagingSenderId: "660442845810",
-  appId: "1:660442845810:web:0981a5669664ce3b17a8eb",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
