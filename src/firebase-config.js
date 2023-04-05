4255e592f9e496382761063d231d6c7f867ccdc2
zachvlat <zachariasvlatakis@gmail.com>
2023-04-05 15:32:47 +0300

    css changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,16 +5,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCh-7TSUhYkV64GqwPxP7oDyqOHy4s7DSs",
+  authDomain: "zatsblog-490c4.firebaseapp.com",
+  projectId: "zatsblog-490c4",
+  storageBucket: "zatsblog-490c4.appspot.com",
+  messagingSenderId: "660442845810",
+  appId: "1:660442845810:web:0981a5669664ce3b17a8eb",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
