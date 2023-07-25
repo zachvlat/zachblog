681bafc347fd3d2f8c42bcbe0fe30603ce31fc2c
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-07-25 16:13:08 +0300

    Update Socials.js

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -3,6 +3,7 @@ import React from "react";
 function Socials() {
   return (
     <div className='socials'>
+      <a hidden rel="me" href="https://infosec.exchange/@zatsando">Mastodon</a>
       <a href='https://github.com/zachvlat'>
         <img src='https://img.icons8.com/nolan/40/github.png' alt='github' />
       </a>
